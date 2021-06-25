4d1c004c63cc3a6e5595be5c67dc927e3dd2656f
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-25 10:23:43 -0700

    test the request

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,66 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "get a list of all the cats" do
+      Cat.create name: 'Cat', age: 3, enjoys: 'Likes to eat alot.'
+
+      get '/cats'
+     
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
+    
+    describe "POST /create" do
+      it "create a cat in the db" do
+        cat_params = {
+          cat: {
+            name: 'Cat',
+            age: 3,
+            enjoys: 'Likes to eat alot.'
+          }
+        }
+
+        post '/cats', params: cat_params
+
+        expect(response).to have_http_status(200)
+
+        cat = Cat.first
+
+        expect(cat.name).to eq 'Cat' 
+        expect(cat.age).to eq 3
+        expect(cat.enjoys).to eq 'Likes to eat alot.'
+      end
+    end
+
+    describe "PATCH /update" do
+    it 'updates a cat' do
+
+      cat_params = {
+        cat: {
+          name: 'Cat',
+          age: 3,
+          enjoys: 'eating'
+        }
+      }
+      post '/cats', params: cat_params
+
+      new_cat_params = {
+        cat: {
+          name: 'Parsely',
+          age: 4,
+          enjoys: 'eating'
+        }
+      }
+
+      cat = Cat.first
+      patch "/cats/#{cat.id}", params: new_cat_params
+
+      expect(response).to have_http_status(200)
+      expect(cat.name).to eq 'Cat'
+      expect(cat.age).to eq 3
+      
+    end
+  end
   end
 end
